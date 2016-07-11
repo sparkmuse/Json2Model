371d613ab7f75c24d790314a0fd92d5c3869d522
Alfredo Lopez <alfredlopezf@hotmail.com>
2016-07-11 14:25:54 -0400

    First commit

---
+++ src/main/java/com/AL/Json2Model/App.java
@@ -0,0 +1,13 @@
+package com.AL.Json2Model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
