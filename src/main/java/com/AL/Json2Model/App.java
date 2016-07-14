68010bf79a8c9fef060829c0289c9a792c45d7db
Alfredo Lopez <alfredlopezf@hotmail.com>
2016-07-13 20:55:37 -0400

    Added the values to the command line arguments.

--- src/main/java/com/AL/Json2Model/App.java
+++ src/main/java/com/AL/Json2Model/App.java
@@ -1,6 +1,9 @@
 package com.al.json2model;
 
+import org.apache.commons.cli.ParseException;
+
 import com.al.json2model.general.Language;
+import com.al.json2model.general.Validator;
 import com.al.json2model.model.ModelJava;
 
 /**
@@ -8,18 +11,30 @@ import com.al.json2model.model.ModelJava;
  *
  */
 public class App {
-	public static void main(String[] args) {
+	
+	public static final String  APP_NAME = "Json2Model";
+	
+	public static void main(String[] args) throws ParseException {
+		
+		// Validate the input
+		Validator v = new Validator(args);
+		v.parse();
+	
+		
+		if (v.isValid()) {
+			
+			// We the Json String
+			Language language = Language.JAVA;
+			String name = "ClassF";
+			//String json = "{ \"f1\":\"Hello\",\"f2\":{\"f3\":\"World\"}}";
+			String json = "{\"widget\":{\"debug\":\"on\",\"window\":{\"title\":\"Sample Konfabulator Widget\",\"name\":\"main_window\",\"width\":500,\"height\":500},\"image\":{\"src\":\"Images/Sun.png\",\"name\":\"sun1\",\"hOffset\":250,\"vOffset\":250,\"alignment\":\"center\"},\"text\":{\"data\":\"Click Here\",\"size\":36,\"style\":\"bold\",\"name\":\"text1\",\"hOffset\":250,\"vOffset\":100,\"alignment\":\"center\"}}}";
+			// a classy way
+			ModelJava m = new ModelJava(name, json, language);
+			m.parse();
+			
+		}
 		
 		
-		// We the Json String
-		Language language = Language.JAVA;
-		String name = "ClassF";
-		//String json = "{ \"f1\":\"Hello\",\"f2\":{\"f3\":\"World\"}}";
-		String json = "{\"widget\":{\"debug\":\"on\",\"window\":{\"title\":\"Sample Konfabulator Widget\",\"name\":\"main_window\",\"width\":500,\"height\":500},\"image\":{\"src\":\"Images/Sun.png\",\"name\":\"sun1\",\"hOffset\":250,\"vOffset\":250,\"alignment\":\"center\"},\"text\":{\"data\":\"Click Here\",\"size\":36,\"style\":\"bold\",\"name\":\"text1\",\"hOffset\":250,\"vOffset\":100,\"alignment\":\"center\"}}}";
-		
-		// a classy way
-		ModelJava m = new ModelJava(name, json, language);
-		m.parse();
 		
 		
 	}
--- src/main/java/com/al/json2model/general/Language.java
+++ src/main/java/com/al/json2model/general/Language.java
@@ -1,11 +1,11 @@
 package com.al.json2model.general;
 
-
 public enum Language {
 	C,
 	OBJECTIVE_C,
 	JAVA,
 	SWIFT,
 	VISUAL_BASIC,
-	VBA
+	VBA,
+	UNKNOWN
 }
