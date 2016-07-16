09aa50cfe00aa38175481d6ec219a421a6afeb82
Alfredo Lopez <alfredlopezf@hotmail.com>
2016-07-16 09:25:53 -0400

    Added comments to the Pluralize class and removed dependencies to the external Pluralization class

--- src/main/java/com/al/json2model/general/Pluralize.java
+++ src/main/java/com/al/json2model/general/Pluralize.java
@@ -2,12 +2,35 @@ package com.al.json2model.general;
 
 import org.apache.commons.lang3.StringUtils;
 
+/**
+ * Class to convert from singular to plural forms in English.
+ * Due to the different variations and complexity of some of the word
+ * rules the algorithms used here might fail in some instances.
+ * 
+ * @author alfredo
+ *
+ */
+
 public class Pluralize {
+	
+	/**
+	 * The word was not found in the IRREGULAR_PLURALS array.
+	 */
+	private static final int INDEX_NOT_FOUND = 0;
+	
+	/**
+	 * The index for the singular's column in the IRREGULAR_PLURALS array.
+	 */
+	private static final int INDEX_SINGULAR = 0;
+	
+	/**
+	 * The index for the plural's column in the IRREGULAR_PLURALS array.
+	 */
+	private static final int INDEX_PLURAL = 1;
 
-	private static final int indexNotFound = 0;
-	private static final int indexSingular = 0;
-	private static final int indexPlural = 1;
-
+	/**
+	 * List of irregular plurals in English.
+	 */
 	private static final String[][] IRREGULAR_PLURALS = { { "addendum", "addenda" }, { "addendum", "addenda" },
 			{ "alga", "algae" }, { "alumna", "alumnae" }, { "alumnus", "alumni" }, { "analysis", "analyses" },
 			{ "antenna", "antennas" }, { "antenna", "antennae" }, { "apparatus", "apparatuses" },
@@ -44,10 +67,21 @@ public class Pluralize {
 
 	private static String word;
 	private static String original;
-	private static int indexIrregular = indexNotFound;
+	private static int indexIrregular = INDEX_NOT_FOUND;
 	private static boolean upper = false;
 	private static boolean capital = false;
 
+	/**
+	 * Procedure to get the plural of a word. We are using the following rules:
+	 * 
+	 * - If the word is found in the IRREGULAR_PLURALS array then use it.
+	 * - If the word ends in 'y' then, drop it and append 'ies'.
+	 * - If the word ends in 's', 'x', 'z', 'ch', 'sh' then, append 'es'
+	 * - For all other cases append 's'
+	 * 
+	 * @param noun The word to pluralize.
+	 * @return A pluralized version of the word.
+	 */
 	public static String getPlural(String noun) {
 
 		String result = null;
@@ -56,10 +90,10 @@ public class Pluralize {
 		setLetterCases();
 		getIrregularPluralIndex();
 
-		if (indexIrregular != indexNotFound) {
+		if (indexIrregular != INDEX_NOT_FOUND) {
 
-			String singular = IRREGULAR_PLURALS[indexIrregular][indexSingular];
-			String plural = IRREGULAR_PLURALS[indexIrregular][indexPlural];
+			String singular = IRREGULAR_PLURALS[indexIrregular][INDEX_SINGULAR];
+			String plural = IRREGULAR_PLURALS[indexIrregular][INDEX_PLURAL];
 
 			result = replaceIrregular(singular, plural);
 
@@ -84,31 +118,16 @@ public class Pluralize {
 
 
 	/**
-	 * @param singular
-	 * @param plural
-	 * @return
+	 * Procedure to get the singular of a word. We are using the following rules:
+	 * 
+	 * - If the word is found in the IRREGULAR_PLURALS array then use it.
+	 * - If the word ends in 'ies' then, drop it and append 'y'.
+	 * - If the word ends in 'ses', 'xes', 'zes', 'ches', 'shes' then drop 'es'.
+	 * - For all other cases drop 's'
+	 * 
+	 * @param noun The word to convert to singular.
+	 * @return A singular version of the word.
 	 */
-	private static String replaceIrregular(String singular, String plural) {
-
-		String result;
-
-		if (upper) {
-
-			singular = singular.toUpperCase();
-			plural = plural.toUpperCase();
-
-		} else if (capital) {
-
-			singular = StringUtils.capitalize(singular);
-			plural = StringUtils.capitalize(plural);
-
-		}
-
-		// Use substitute in case we have compound words of irregulars.
-		result = original.replaceAll(singular, plural);
-		return result;
-	}
-
 	public static String getSingular(String noun) {
 
 		String result = null;
@@ -117,10 +136,10 @@ public class Pluralize {
 		setLetterCases();
 		getIrregularSingularIndex();
 
-		if (indexIrregular != indexNotFound) {
+		if (indexIrregular != INDEX_NOT_FOUND) {
 
-			String singular = IRREGULAR_PLURALS[indexIrregular][indexSingular];
-			String plural = IRREGULAR_PLURALS[indexIrregular][indexPlural];
+			String singular = IRREGULAR_PLURALS[indexIrregular][INDEX_SINGULAR];
+			String plural = IRREGULAR_PLURALS[indexIrregular][INDEX_PLURAL];
 
 			result = replaceIrregular(plural, singular);
 
@@ -140,24 +159,76 @@ public class Pluralize {
 
 		return result;
 	}
+	
+	
+	/**
+	 * Helper method to replace one word from the other. This method allows
+	 * to expand the 'Irregular' array to include compound words of those
+	 * irregulars.
+	 * 
+	 * Ex: AmazingChild -> AmazingChildren
+	 * 
+	 * @param singular The word in singular
+	 * @param plural The equivalent word in plural.
+	 * @return The final word with the strings swapped.
+	 */
+	private static String replaceIrregular(String singular, String plural) {
 
+		String result;
+
+		if (upper) {
+
+			singular = singular.toUpperCase();
+			plural = plural.toUpperCase();
+
+		} else if (capital) {
+
+			singular = StringUtils.capitalize(singular);
+			plural = StringUtils.capitalize(plural);
+
+		}
+
+		result = original.replaceAll(singular, plural);
+		return result;
+	}
+
+	/**
+	 * Helper method to do the casing for the final words in the class.
+	 * We want to preserve the original casing rules from the original
+	 * as much as possible.
+	 * 
+	 * If all letters in the word are UPPER case then we want to use UPPER case.
+	 * If the first letter of the word is Upper we Capitalize it.
+	 * Otherwise, we keep lower case.
+	 */
 	private static void setLetterCases() {
 
 		upper = StringUtils.isAllUpperCase(original);
 		capital = StringUtils.capitalize(original).equals(original);
 	}
 
+	/**
+	 * Get the irregular plural of a word.
+	 */
 	private static void getIrregularPluralIndex() {
-		indexForWord(indexSingular);
+		indexForWord(INDEX_SINGULAR);
 	}
 
+	/**
+	 * Get the irregular singular of a word.
+	 */
 	private static void getIrregularSingularIndex() {
-		indexForWord(indexPlural);
+		indexForWord(INDEX_PLURAL);
 	}
 
+	/**
+	 * Helper method to find an irregular word in the IRREGULAR_PLURALS array.
+	 * 
+	 * @param indexToCheck if we want to check plural's or singular's dimension.
+	 */
 	private static void indexForWord(int indexToCheck) {
 
-		indexIrregular = indexNotFound;
+		indexIrregular = INDEX_NOT_FOUND;
 		for (int i = 0; i < IRREGULAR_PLURALS.length; i++) {
 			if (word.endsWith(IRREGULAR_PLURALS[i][indexToCheck])) {
 				indexIrregular = i;
